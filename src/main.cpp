71e264a412c3e0d07a3c99d7b8c03291fcbda20b
tsaruggan <tsaruggan@gmail.com>
2024-10-29 19:39:40 -0400

    hello world

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Squishy
+//
+//  Created by Saruggan Thiruchelvan on 2024-10-29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
